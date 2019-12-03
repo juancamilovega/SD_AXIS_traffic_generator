82c6208973dd435180ce766a560b7421efca8e4d
Qianfeng Shen <qianfeng.shen@gmail.com>
2019-12-03 23:22:32 +0000

    commit before stable

--- sw/driver/user_def.h
+++ sw/driver/user_def.h
@@ -5,15 +5,22 @@
 //#define USE_AXIREGS 
 
 /* -----------------------------------------------------
--  set the mmap base, have a look at your vivado register
--  editor before set this
+-  set the mmap base and ip mapped addresses
+-  have a look at your vivado register editor before set this
    ----------------------------------------------------- */
-#define MAP_BASE 0xA0000000
-#define MAP_SIZE 0x10000
+#define MAP_BASE 0xA0020000
+#define MAP_SIZE 0x20000
 #define DMA_BUFFER_SIZE 4*1024*1024 //4MB
-#define DMA_OFFSET 0x1000
+#define DMA_OFFSET 0
+#define TRAFFIC_GEN_OFFSET 0x10000
 
 /* -----------------------------------------------------
 -  choose which AXIDMA to use when there are multiple
    ----------------------------------------------------- */
 #define PS_DMA_CHANNEL_ID 0
+
+/* -----------------------------------------------------
+-  clock period
+   ----------------------------------------------------- */
+
+#define CLOCK_PERIOD 4
