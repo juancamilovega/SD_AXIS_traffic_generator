edb70b189ba1ae507e81f7c0f9d472622b57e20b
Qianfeng Shen <qianfeng.shen@gmail.com>
2019-12-03 03:20:02 +0000

    hw code clean up for arm

--- hw/ip_repo/full_core/scripts/assemble.tcl
+++ hw/ip_repo/full_core/scripts/assemble.tcl
@@ -221,5 +221,5 @@ update_ip_catalog
 ipx::merge_project_changes files [ipx::current_core]
 ipx::check_integrity -quiet [ipx::current_core]
 ipx::archive_core $project_dir/$project_name/${project_name}.srcs/sources_1/bd/${project_name}/${project_name}_1.0.zip [ipx::current_core]
-#close_project
-#exit
+close_project
+exit
