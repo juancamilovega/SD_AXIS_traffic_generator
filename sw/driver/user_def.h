e296bebc74f9ac7d87931b61449149073e65cd34
Clark <qianfeng.shen@gmail.com>
2020-12-11 15:19:28 -0500

    fix user def file

--- sw/driver/user_def.h
+++ sw/driver/user_def.h
@@ -8,13 +8,8 @@
 -  set the mmap base and ip mapped addresses
 -  have a look at your vivado register editor before set this
    ----------------------------------------------------- */
-<<<<<<< HEAD
-#define MAP_BASE 0xA0020000
-#define MAP_SIZE 0x30000
-=======
 #define MAP_BASE 0xA0000000
 #define MAP_SIZE 0x10000
->>>>>>> 59c9277b79fb1aa0bdf601e83f74ca1ac127d576
 #define DMA_BUFFER_SIZE 4*1024*1024 //4MB
 #define DMA_OFFSET 0x2000
 #define TRAFFIC_GEN_OFFSET 0x0
@@ -27,9 +22,4 @@
 /* -----------------------------------------------------
 -  clock period
    ----------------------------------------------------- */
-<<<<<<< HEAD
-
-#define CLOCK_PERIOD 4.5829
-=======
 #define CLOCK_PERIOD 5
->>>>>>> 59c9277b79fb1aa0bdf601e83f74ca1ac127d576
